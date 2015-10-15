9348e16b686d296610c2dedefe7ea5bb37a89a1e
Vighnesh Birodkar <vighneshbirodkar@nyu.edu>
2015-10-15 16:57:03 -0400

    Fixed elkans kmeans so that predict is same as labels

--- sklearn/cluster/tests/test_k_means.py
+++ sklearn/cluster/tests/test_k_means.py
@@ -692,6 +692,11 @@ def test_predict_equal_labels():
     km.fit(X)
     assert_array_equal(km.predict(X), km.labels_)
 
+    km = KMeans(random_state=13, n_jobs=1, n_init=1, max_iter=1,
+                algorithm='triangle_inequality')
+    km.fit(X)
+    assert_array_equal(km.predict(X), km.labels_)
+
 
 def test_n_init():
     # Check that increasing the number of init increases the quality
