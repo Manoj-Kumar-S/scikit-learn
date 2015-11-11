49d3a4ec851ff9de121e9c5c4ad833cc94cd9782
MechCoder <manojkumarsivaraj334@gmail.com>
2015-11-11 12:48:01 -0500

    Add documentation and inline_comments to _k_means_elkan.pyx

--- sklearn/cluster/_k_means_elkan.pyx
+++ sklearn/cluster/_k_means_elkan.pyx
@@ -30,6 +30,20 @@ cdef update_labels_distances_inplace(
         double* X, double* centers, double[:, :] center_distances, int[:] labels,
         double[:, :] lower_bounds, double[:] upper_bounds, int n_samples, int
         n_features, int n_clusters):
+    """
+    Given X, centers and the pairwise distances divided by 2.0 between the centers
+    this calculates the upper bounds and lower bounds for each sample.
+
+    The upper bound for each sample is set to the distance between the sample and the
+    closest center.
+
+    The lower bound for each sample is a one-dimensional array of n_clusters.
+    For each sample i assume that the previously assigned cluster is c1 and the previous
+    closest distance is dist, for a new cluster c2, the lower_bound[i][c2] is set to distance
+    between the sample and this new cluster, if and only if dist > center_distances[c1][c2].
+    This prevents computation of unnecessary distances for each sample to the clusters
+    that it is unlikely to be assigned to.
+    """
     # assigns closest center to X
     # uses triangle inequality
     cdef double* x
@@ -95,6 +109,8 @@ def k_means_elkan(np.ndarray[np.float64_t, ndim=2, mode='c'] X_, int n_clusters,
     cdef int[:] labels = labels_
     upper_bounds_ = np.empty(n_samples, dtype=np.float)
     cdef double[:] upper_bounds = upper_bounds_
+
+    # Get the inital set of upper bounds and lower bounds for each sample.
     update_labels_distances_inplace(X_p, centers_p, center_distances, labels,
                                     lower_bounds, upper_bounds, n_samples,
                                     n_features, n_clusters)
@@ -109,29 +125,44 @@ def k_means_elkan(np.ndarray[np.float64_t, ndim=2, mode='c'] X_, int n_clusters,
     for iteration in range(max_iter):
         if verbose:
             print("start iteration")
+
         # we could find the closest center in O(n) but
         # this does not seem to be the bottleneck
         distance_next_center = np.sort(center_distances, axis=0)[1]
         if verbose:
             print("done sorting")
+
         for point_index in range(n_samples):
             upper_bound = upper_bounds[point_index]
             label = labels[point_index]
+
+            # This means that the next likely center is far away from the
+            # currently assigned center and the sample is unlikely to be
+            # reassigned.
             if distance_next_center[label] >= upper_bound:
                 continue
             x_p = X_p + point_index * n_features
-            # check other update conditions
+
             # TODO: get pointer to lower_bounds[point_index, center_index]
             for center_index in range(n_clusters):
+
+                # If this holds, then center_index is a good candidate for the
+                # sample to be relabelled, and we need to confirm this by
+                # recomputing the upper and lower bounds.
                 if (center_index != label
                         and (upper_bound > lower_bounds[point_index, center_index])
                         and (upper_bound > center_distances[center_index, label])):
-                    # update distance to center
+
+                    # Recompute the upper bound by calculating the actual distance
+                    # between the sample and label.
                     if not bounds_tight[point_index]:
                         upper_bound = euclidian_dist(x_p, centers_p + label * n_features, n_features)
                         lower_bounds[point_index, label] = upper_bound
                         bounds_tight[point_index] = 1
-                    # check for relabels
+
+                    # If the condition still holds, then compute the actual distance between
+                    # the sample and center_index. If this is still lesser than the previous
+                    # distance, reassign labels.
                     if (upper_bound > lower_bounds[point_index, center_index]
                             or (upper_bound > center_distances[label, center_index])):
                         distance = euclidian_dist(x_p, centers_p + center_index * n_features, n_features)
@@ -139,22 +170,28 @@ def k_means_elkan(np.ndarray[np.float64_t, ndim=2, mode='c'] X_, int n_clusters,
                         if distance < upper_bound:
                             label = center_index
                             upper_bound = distance
+
             labels[point_index] = label
             upper_bounds[point_index] = upper_bound
+
         if verbose:
             print("end inner loop")
+
         # compute new centers
         new_centers = _centers_dense(X_, labels_, n_clusters, upper_bounds_)
         bounds_tight[:] = 0
 
         # compute distance each center moved
         center_shift = np.sqrt(np.sum((centers_ - new_centers) ** 2, axis=1))
+
         # update bounds accordingly
         lower_bounds = np.maximum(lower_bounds - center_shift, 0)
         upper_bounds = upper_bounds + center_shift[labels_]
+
         # reassign centers
         centers_ = new_centers
         centers_p = <double*>new_centers.data
+
         # update between-center distances
         center_distances = euclidean_distances(centers_) / 2.
         if verbose:
@@ -166,6 +203,9 @@ def k_means_elkan(np.ndarray[np.float64_t, ndim=2, mode='c'] X_, int n_clusters,
                 print("center shift %e within tolerance %e"
                       % (center_shift_total, tol))
             break
+
+    # We need this to make sure that the labels give the same output as
+    # predict(X)
     if center_shift_total > 0:
         update_labels_distances_inplace(X_p, centers_p, center_distances,
                                         labels, lower_bounds, upper_bounds,
